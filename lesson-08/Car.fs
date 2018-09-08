853d3141743ba60fd9af78d51dcd7da5f025aa0f
mje2 <matteskolin@gmail.com>
2018-09-08 14:08:54 -0500

    complete capstone1

--- lesson-08/Car.fs
+++ lesson-08/Car.fs
@@ -2,7 +2,25 @@
 
 open System
 
+let getDistance (destination) =
+    if destination = "Gas" then 10
+    elif destination = "Home" then 25
+    elif destination = "Office" then 50
+    elif destination = "Stadium" then 25
+    else failwith "Unknown destination!"
+
+let calculateRemainingPetrol(currentPetrol:int, distance:int) : int = 
+    if currentPetrol < distance then failwith "Oops! You've run out of Petrol"
+    let remainingPetrol = currentPetrol - distance
+    remainingPetrol    
 //TODO: Create helper functions to provide the building blocks to implement driveTo.
 
 /// Drives to a given destination given a starting amount of petrol
-let driveTo (petrol, destination) = petrol
+let driveTo(petrol:int, destination:string) :int = 
+    let distance = getDistance(destination)
+    let remainingPetrol = calculateRemainingPetrol(petrol,distance)
+    if destination = "Gas" then remainingPetrol + 50
+    else remainingPetrol
+
+
+
--- lesson-08/scratchpad.fsx
+++ lesson-08/scratchpad.fsx
@@ -3,10 +3,37 @@ open System
 /// Gets the distance to a given destination 
 let getDistance (destination) =
     if destination = "Gas" then 10
-    // fill in the blanks!
+    elif destination = "Home" then 25
+    elif destination = "Office" then 50
+    elif destination = "Stadium" then 25
     else failwith "Unknown destination!"
 
 // Couple of quick tests
 getDistance("Home") = 25
-getDistance("Stadium") = 25
+getDistance("Stadium") =25
+
+
+let calculateRemainingPetrol(currentPetrol:int, distance:int) : int = 
+    if currentPetrol < distance then failwith "Oops! You've run out of Petrol"
+    let remainingPetrol = currentPetrol - distance
+    remainingPetrol    
+
+calculateRemainingPetrol(4,30)
+
+let distanceToGas = getDistance("Gas")
+calculateRemainingPetrol(25, distanceToGas)
+calculateRemainingPetrol(5,distanceToGas)
+
+let driveTo(petrol:int, destination:string) :int = 
+    let distance = getDistance(destination)
+    let remainingPetrol = calculateRemainingPetrol(petrol,distance)
+    if destination = "Gas" then remainingPetrol + 50
+    else remainingPetrol
+
+let a = driveTo(100,"Office")
+let b = driveTo(a, "Stadium")
+let c = driveTo(b, "Gas")
+let _answer = driveTo(c,"Home")
+    
+
 
