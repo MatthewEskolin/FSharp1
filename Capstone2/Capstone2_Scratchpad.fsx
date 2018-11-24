fd046b9981cd59a8696c9453e674dbad63ac3404
mje2 <matteskolin@gmail.com>
2018-11-24 10:50:28 -0600

    start work on capstone2

--- Capstone2/Capstone2_Scratchpad.fsx
+++ Capstone2/Capstone2_Scratchpad.fsx
@@ -1,1 +1,31 @@
-﻿
+﻿//Create Records
+type Customer = { Name : string }
+type Account = { AccountId: System.Guid; Owner: Customer; Balance : decimal }
+
+///Deposits an amount into an account
+let deposit (amount:decimal) (account:Account) : Account = 
+    let newBalance = account.Balance + amount
+    let newAccount = { account with Balance = newBalance}
+    newAccount 
+
+
+let withdrawl (amount:decimal) (account:Account) : Account = 
+    if(amount > account.Balance) 
+        then account 
+    else
+        let newBalance = account.Balance - amount
+        let newAccount = { account with Balance = newBalance }
+        newAccount
+        
+
+let testCustomer : Customer = { Name = "John Smith"}
+let testCustomer1 : Customer = { Name = "Mike Johnson"}
+let account1 : Account = { AccountId = System.Guid.NewGuid(); Owner = testCustomer; Balance = 5000M;}
+
+
+    
+
+
+
+
+
