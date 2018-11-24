fcdddab36b3e175471236caa533163f9ca314bdb
mje2 <matteskolin@gmail.com>
2018-11-24 15:23:48 -0600

    prepare to pull into compiled application

--- Capstone2/Capstone2_Scratchpad.fsx
+++ Capstone2/Capstone2_Scratchpad.fsx
@@ -9,7 +9,7 @@ let deposit (amount:decimal) (account:Account) : Account =
     newAccount 
 
 
-let withdrawl (amount:decimal) (account:Account) : Account = 
+let withdraw (amount:decimal) (account:Account) : Account = 
     if(amount > account.Balance) 
         then account 
     else
@@ -24,6 +24,57 @@ let account1 : Account = { AccountId = System.Guid.NewGuid(); Owner = testCustom
 
 
     
+//File System Logger
+let fileSystemAudit account message = 
+    let directoryName = sprintf @"C:\temp\learnfs\capstone2\%s" account.Owner.Name 
+    printfn "%s" directoryName
+    let logDirectory = System.IO.Directory.CreateDirectory(directoryName)
+    let writer = System.IO.File.AppendText(sprintf @"%s\%A" directoryName account.AccountId)
+    let result = writer.WriteLine(sprintf "Account %A:%s" account.AccountId message)
+    let result = writer.WriteLine(sprintf "Account %A:%s" account.AccountId message)
+    let result = writer.WriteLine(sprintf "Account %A:%s" account.AccountId message)
+    writer.Flush() |> ignore
+    
+
+
+let consoleAudit account message = 
+    let formattedMessage = sprintf "Account %A:%s" account.AccountId message
+    printfn "%s" formattedMessage
+
+
+
+let customer = { Name = "Issac" }
+let account = {AccountId = System.Guid.Empty; Owner = customer; Balance = 90M}
+
+//Test out withdraw
+let newAccount = account |> withdraw 10M
+newAccount.Balance = 80M
+
+//Test out console auditor
+consoleAudit account "Testing console Audit"
+
+let auditAs (operationName:string) (audit:Account -> string -> unit)
+    (operation:decimal -> Account -> Account) (amount:decimal)
+    (account:Account) : Account = 
+        //do operation
+        let accountResult = operation amount account
+        let auditMessage  = sprintf "Performing an %s operation for $%M..." operationName amount
+        audit account auditMessage |> ignore
+        accountResult
+        //audit operation
+
+
+
+
+
+
+//try using "Curried Function"
+let withdrawWithConsoleAudit = auditAs "withdraw" consoleAudit withdraw
+let depositWithConsoleAudit = auditAs "deposit" consoleAudit deposit
+
+
+    
+
 
 
 
