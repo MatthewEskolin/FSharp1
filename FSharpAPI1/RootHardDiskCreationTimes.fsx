c4f2d2640fd6d81f1f434b6056361cf2aa3821e7
mje2 <matteskolin@gmail.com>
2018-12-27 12:42:17 -0600

    NYT - hard disk work

--- FSharpAPI1/RootHardDiskCreationTimes.fsx
+++ FSharpAPI1/RootHardDiskCreationTimes.fsx
@@ -1,1 +1,10 @@
-﻿
+﻿open System
+open System.IO
+
+let dirs = System.IO.Directory.EnumerateDirectories "C:/"
+let dirsInfos = dirs |> Seq.map(fun x -> new DirectoryInfo(x))
+let directoryMap = dirsInfos|> Seq.map(fun x -> x.Name,x.CreationTimeUtc) |> Map.ofSeq
+let directoryAges = directoryMap |> Map.map(fun x  y -> (DateTime.Now - directoryMap.[x]).TotalDays)
+
+
+
